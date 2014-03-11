58ab2840f06df98c7081b160287efdc4ea99849e
Matthias Dunkel <md@ivaris.ch>
2014-03-11 17:27:15 +0100

    Tried to figure out a better encryption for the data. This Commit contains the tests for that.

--- start.php
+++ start.php
@@ -1,5 +1,24 @@
 <?php
 
+/* ENCRYPTION TEST:
+$ivSize = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+$iv = mcrypt_create_iv($ivSize, MCRYPT_RAND);
+
+$key = openssl_random_pseudo_bytes(32);
+//$hex   = bin2hex($bytes);
+//echo $hex;
+$data = "lkasdjlkJ lkjlkj lkj";
+$enc = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_CBC, $iv);
+echo $enc;
+echo "\n";
+$dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $enc, MCRYPT_MODE_CBC, $iv);
+echo $dec;
+echo "\n";
+
+echo bin2hex($iv);
+echo "\n";
+
+exit; */
 require_once("vendor/BitcasaClient.php");
 require_once("vendor/KLogger.php");
 
